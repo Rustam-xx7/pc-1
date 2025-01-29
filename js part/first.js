a7282b6f5b52b570d78bd5dffe3bbcad2ecb1a53
Rustam-xx7 <chakrabortyrustam@gmail.com>
2025-01-29 10:45:39 +0530

    29-01-25 , 2

---
+++ js part/first.js
@@ -0,0 +1,9 @@
+// This is a basic JavaScript program
+
+// Function to print a greeting message
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Call the function with a sample name
+greet("World");
