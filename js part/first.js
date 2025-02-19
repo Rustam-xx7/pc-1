705ad388842a8c42b3d44888c5cea2391387154e
Rustam-xx7 <chakrabortyrustam@gmail.com>
2025-02-19 20:44:38 +0530

    198-02-25, 1

--- js part/first.js
+++ js part/first.js
@@ -1,9 +1,20 @@
-// This is a basic JavaScript program
-
-// Function to print a greeting message
-function greet(name) {
-    console.log("Hello, " + name + "!");
-}
-
-// Call the function with a sample name
-greet("World");
+console.log("Welcome to reality!");
+// variables
+let age = 25;
+age = 26; // let can be update , but not declare again.
+console.log(age);
+const x = null; // cant be redeclared or update .
+console.log(x);
+let price;
+price = 19.90;
+console.log(price);
+const student = {
+    name: "John",
+    age: 25,
+    isStudent: true ,
+} ;
+console.log(student);
+console.log(student["name"]);
+// student["age"] = student["age"] +1;
+console.log(student["age"] + 1);
+console.log(typeof student["name"]);
