836dc0d21c2942912f9870d5d0edfcdf1ee0bfd1
Rustam-xx7 <chakrabortyrustam@gmail.com>
2025-02-20 18:18:50 +0530

    20-02-25, 1

--- js part/first.js
+++ js part/first.js
@@ -18,3 +18,37 @@ console.log(student["name"]);
 // student["age"] = student["age"] +1;
 console.log(student["age"] + 1);
 console.log(typeof student["name"]);
+// arithmatic operators
+let a = 5;
+let b = 3;
+let c = "10";
+console.log("a = ",a ,"b = ",b , "c = (string)", c);
+console.log("a+b = ", a + b);
+console.log("a*b = ", a * b);
+console.log("a**b = ", a ** b); // a^b exponation
+console.log("a++ = ", a++ ); // post incriment operator
+console.log(" now a = ", a ); // after incriment operator
+console.log("a+= 4 = ", a+=4);
+console.log("now a is =", a); // a = a+4
+//comparition operator 
+console.log("a == b " , a==b ); //false 
+console.log("a == c " , a==c ); // true , string>>> number ==number. 
+console.log("a === c " , a===c ); // false , equal to and type .
+
+console.log(" conditional statements ");
+{
+    let mode =  "dark";
+    let color;
+    if ( mode === "dark") {
+        color = "black";
+    } else {
+        color = "white";
+    }
+    console.log(mode);
+    console.log(color);
+}
+// tournary operator
+{
+    let age=25;
+    age >= 18 ? console.log(age,"addutlt") : console.log(age,"child");
+}
