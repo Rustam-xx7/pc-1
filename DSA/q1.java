385e8994b3af20bae239c7be69203d3214c46f9d
Rustam-xx7 <chakrabortyrustam@gmail.com>
2026-06-23 13:04:04 +0530

    dsa day 1

--- DSA/q1.java
+++ DSA/q1.java
@@ -33,11 +33,29 @@ public class q1 {
         return rev;
     }
 
+    static boolean isPalindrome(int n) {
+        return n == reverceNumber(n);
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        int n = 12345;
-        System.out.println(countDigits(n));
-        System.out.println(sumOfDigits(n));
-        System.out.println(reverceNumber(n));
+        int n = 13;
+        System.out.println(n + " Number of digits: " + countDigits(n));
+        System.out.println(n + " Sum of digits: " + sumOfDigits(n));
+        System.out.println(n + " Reversed: " + reverceNumber(n));
+        System.out.println(n + " Is a palindrome: " + isPalindrome(n));
+        System.out.println(n + " Is a prime number: " + isPrime(n));
 
     }
 }
